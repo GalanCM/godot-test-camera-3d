8a7df64062f162dc5ffe00febd9509938b7634fc
Galan Montgomery <galan.c.m@gmail.com>
2023-04-27 10:19:46 -0400

    Improvements to settings initialization
    - Non-InputMap settings aren't saved when using the default value,
    and won't show up if not saved. They're now added
    on _enter_tree instead of _enable_plugin.
    - Added initial_values to InputMap setting,
    even though they seem non-functional.

--- addons/test_camera_3d/test_camera_plugin.gd
+++ addons/test_camera_3d/test_camera_plugin.gd
@@ -12,7 +12,21 @@ enum CameraAngles {
 }
 
 
-func _enable_plugin():
+func _enter_tree() -> void:
+	if Engine.is_editor_hint():
+		add_autoload_singleton("TestCameraManager", "res://addons/test_camera_3d/test_camera_manager.gd")
+
+		initialize_settings()
+
+
+func _exit_tree() -> void:
+	if Engine.is_editor_hint():
+		remove_autoload_singleton("TestCameraManager")
+
+
+func initialize_settings() -> void:
+	# keymap
+
 	var create_default_key := func(physical_keycode: int, include_shift: bool = false) -> InputEventKey:
 		var default_key = InputEventKey.new()
 		default_key.ctrl_pressed = true
@@ -24,22 +38,34 @@ func _enable_plugin():
 
 	if not ProjectSettings.has_setting("input/testcamera_up"):
 		ProjectSettings.set_setting("input/testcamera_up", {"deadzone": 0.5, "events": [create_default_key.call(KEY_UP)]})
+		ProjectSettings.set_initial_value("input/testcamera_up", {"deadzone": 0.5, "events": [create_default_key.call(KEY_UP)]})
 	if not ProjectSettings.has_setting("input/testcamera_down"):
 		ProjectSettings.set_setting("input/testcamera_down", {"deadzone": 0.5, "events": [create_default_key.call(KEY_DOWN)]})
+		ProjectSettings.set_initial_value("input/testcamera_down", {"deadzone": 0.5, "events": [create_default_key.call(KEY_DOWN)]})
 	if not ProjectSettings.has_setting("input/testcamera_left"):
 		ProjectSettings.set_setting("input/testcamera_left", {"deadzone": 0.5, "events": [create_default_key.call(KEY_LEFT)]})
+		ProjectSettings.set_initial_value("input/testcamera_left", {"deadzone": 0.5, "events": [create_default_key.call(KEY_LEFT)]})
 	if not ProjectSettings.has_setting("input/testcamera_right"):
 		ProjectSettings.set_setting("input/testcamera_right", {"deadzone": 0.5, "events": [create_default_key.call(KEY_RIGHT)]})
+		ProjectSettings.set_initial_value("input/testcamera_right", {"deadzone": 0.5, "events": [create_default_key.call(KEY_RIGHT)]})
 	if not ProjectSettings.has_setting("input/testcamera_in"):
 		ProjectSettings.set_setting("input/testcamera_in", {"deadzone": 0.5, "events": [create_default_key.call(KEY_UP, true)]})
+		ProjectSettings.set_initial_value("input/testcamera_in", {"deadzone": 0.5, "events": [create_default_key.call(KEY_UP, true)]})
 	if not ProjectSettings.has_setting("input/testcamera_out"):
 		ProjectSettings.set_setting("input/testcamera_out", {"deadzone": 0.5, "events": [create_default_key.call(KEY_DOWN, true)]})
+		ProjectSettings.set_initial_value("input/testcamera_out", {"deadzone": 0.5, "events": [create_default_key.call(KEY_DOWN, true)]})
 	if not ProjectSettings.has_setting("input/testcamera_follow_toggle"):
 		ProjectSettings.set_setting("input/testcamera_follow_toggle", {"deadzone": 0.5, "events": [create_default_key.call(KEY_QUOTELEFT)]})
+		ProjectSettings.set_initial_value("input/testcamera_follow_toggle", {"deadzone": 0.5, "events": [create_default_key.call(KEY_QUOTELEFT)]})
 	if not ProjectSettings.has_setting("input/testcamera_grid_toggle"):
 		ProjectSettings.set_setting("input/testcamera_grid_toggle", {"deadzone": 0.5, "events": [create_default_key.call(KEY_BACKSLASH)]})
+		ProjectSettings.set_initial_value("input/testcamera_grid_toggle", {"deadzone": 0.5, "events": [create_default_key.call(KEY_BACKSLASH)]})
 
-	var quick_inflect = func(keys: Array):
+	# plugin settings
+
+	# Starting Angle
+
+	var quick_inflect = func(keys: Array) -> Array:
 		var inflected: PackedStringArray = []
 		for string in keys:
 			string = string as String
@@ -59,15 +85,3 @@ func _enable_plugin():
 			"hint": PROPERTY_HINT_ENUM,
 			"hint_string": ",".join(quick_inflect.call(CameraAngles.keys()))
 		})
-
-	ProjectSettings.save.call_deferred()
-
-
-func _enter_tree():
-	if Engine.is_editor_hint():
-		add_autoload_singleton("TestCameraManager", "res://addons/test_camera_3d/test_camera_manager.gd")
-
-
-func _exit_tree():
-	if Engine.is_editor_hint():
-		remove_autoload_singleton("TestCameraManager")
